823326fa0b01b0839f1328c0ea0a8b58f033d1a5
Toshiki Takakubo <219tosiki@gmail.com>
2024-06-06 16:23:51 +0900

    feat: add Dockerfile, docker-compose.yml, and launch scripts

---
+++ app.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+print("Hello, Docker Compose!")
+print("Numpy version:", np.__version__)
+print("Pandas version:", pd.__version__)
---
+++ launch_python_shell.ps1
@@ -0,0 +1,7 @@
+# PowerShell script equivalent
+
+# Start Docker containers in detached mode
+docker-compose up -d
+
+# Execute a bash shell in the python-env container
+docker exec -it python-env bash
